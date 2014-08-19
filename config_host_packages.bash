3664640f3a25fefc4b96a48a706fa97aa3db5174
John Mark Schofield <jms@schof.org>
2014-08-19 12:01:17 -0700

    Trying to keep up with ansible code changes. Creating secure passwords for all in user_variables.yml.

--- config_host_packages.bash
+++ config_host_packages.bash
@@ -8,17 +8,13 @@ set -x
 
 ssh -o StrictHostKeyChecking=no root@$PUBLIC_IP "true"
 
+ssh root@$PUBLIC_IP "cp /etc/apt/sources.list /etc/apt/sources.list.original"
 ssh root@$PUBLIC_IP "echo deb [arch=amd64] http://dc0e2a2ef0676c3453b1-31bb9324d3aeab0d08fa434012c1e64d.r5.cf1.rackcdn.com LA main > /etc/apt/sources.list"
 ssh root@$PUBLIC_IP "curl http://dc0e2a2ef0676c3453b1-31bb9324d3aeab0d08fa434012c1e64d.r5.cf1.rackcdn.com/repo.gpg | apt-key add -"
 ssh root@$PUBLIC_IP "apt-get update"
 ssh root@$PUBLIC_IP "DEBIAN_FRONTEND=noninteractive apt-get install -q -y update-notifier-common"
 ssh root@$PUBLIC_IP "DEBIAN_FRONTEND=noninteractive apt-get -q -y dist-upgrade"
-ssh root@$PUBLIC_IP "test -e /var/run/reboot-required && reboot || true"
-sleep 5
 
-set +e
-while ! ssh root@$PUBLIC_IP ls ; do sleep 5 ; done
-set -e
 
 # Disk stuff
 ssh root@$PUBLIC_IP "DEBIAN_FRONTEND=noninteractive apt-get -q -y install lvm2 parted"
@@ -27,7 +23,7 @@ ssh root@$PUBLIC_IP "DEBIAN_FRONTEND=noninteractive apt-get -q -y install lvm2 p
 ssh root@$PUBLIC_IP "DEBIAN_FRONTEND=noninteractive apt-get -q -y install bridge-utils"
 
 # Build Requirements
-ssh root@$PUBLIC_IP "DEBIAN_FRONTEND=noninteractive apt-get -q -y install git aptitude python-dev python-setuptools"
+ssh root@$PUBLIC_IP "DEBIAN_FRONTEND=noninteractive apt-get -q -y install git aptitude python-dev python-setuptools pwgen"
 
 # Not-crappy editors + tools
 ssh root@$PUBLIC_IP "DEBIAN_FRONTEND=noninteractive apt-get -q -y install emacs24-nox vim tmux"
@@ -37,3 +33,10 @@ ssh root@$PUBLIC_IP "easy_install pip"
 
 #install ansible
 ssh root@$PUBLIC_IP "pip install ansible==1.6.6"
+
+ssh root@$PUBLIC_IP "test -e /var/run/reboot-required && reboot || true"
+sleep 5
+
+set +e
+while ! ssh root@$PUBLIC_IP ls ; do sleep 5 ; done
+set -e
--- install_openstack.bash
+++ install_openstack.bash
@@ -3,7 +3,7 @@
 set -e
 set -o pipefail
 set -u
-source cloudenv
+source /root/cloudenv
 set -x
 
 USERVARFILE=/etc/rpc_deploy/user_variables.yml
@@ -21,11 +21,57 @@ cp /root/rpc_user_config.yml /etc/rpc_deploy/
 
 # Change user_variables.yml
 sed -i "s|rackspace_cloud_auth_url:.*|rackspace_cloud_auth_url: $OS_AUTH_URL|g" $USERVARFILE
-sed -i "s|rackspace_cloud_tenant_id:.*|rackspace_cloud_tenant_id: $OS_TENANT_NAME"
+sed -i "s|rackspace_cloud_tenant_id:.*|rackspace_cloud_tenant_id: $OS_TENANT_NAME|g" $USERVARFILE
+sed -i "s|rackspace_cloud_username:.*|rackspace_cloud_username: $OS_USERNAME|g" $USERVARFILE
+sed -i "s|rackspace_cloud_password:.*|rackspace_cloud_password: $RACKSPACE_LOGIN_PW|g" $USERVARFILE
+sed -i "s|rackspace_cloud_api_key:.*|rackspace_cloud_api_key: $OS_PASSWORD|g" $USERVARFILE
+
+sed -i "s|rabbitmq_password:.*|rabbitmq_password: `pwgen 32 1`|g" $USERVARFILE
+sed -i "s|rabbitmq_cookie_token:.*|rabbitmq_cookie_token: `pwgen 32 1`|g" $USERVARFILE
+
+sed -i "s|memcached_encryption_key:.*|memcached_encryption_key: `pwgen 32 1`|g" $USERVARFILE
+
+sed -i "s|container_openstack_password:.*|container_openstack_password: `pwgen 32 1`|g" $USERVARFILE
+
+sed -i "s|mysql_root_password:.*|mysql_root_password: `pwgen 32 1`|g" $USERVARFILE
+sed -i "s|mysql_debian_sys_maint_password:.*|mysql_debian_sys_maint_password: `pwgen 32 1`|g" $USERVARFILE
+
+sed -i "s|keystone_container_mysql_password:.*|keystone_container_mysql_password: `pwgen 32 1`|g" $USERVARFILE
+sed -i "s|keystone_auth_admin_token:.*|keystone_auth_admin_token: `pwgen 32 1`|g" $USERVARFILE
+sed -i "s|keystone_auth_admin_password:.*|keystone_auth_admin_password: `pwgen 32 1`|g" $USERVARFILE
+sed -i "s|keystone_service_password:.*|keystone_service_password: `pwgen 32 1`|g" $USERVARFILE
+
+sed -i "s|cinder_container_mysql_password:.*|cinder_container_mysql_password: `pwgen 32 1`|g" $USERVARFILE
+sed -i "s|cinder_service_password:.*|cinder_service_password: `pwgen 32 1`|g" $USERVARFILE
+sed -i "s|cinder_v2_service_password:.*|cinder_v2_service_password: `pwgen 32 1`|g" $USERVARFILE
+
 sed -i "s|glance_default_store:.*|glance_default_store: file|g" $USERVARFILE
+sed -i "s|glance_container_mysql_password:.*|glance_container_mysql_password: `pwgen 32 1`|g" $USERVARFILE
+sed -i "s|glance_service_password:.*|glance_service_password: `pwgen 32 1`|g" $USERVARFILE
 
+sed -i "s|heat_stack_domain_admin_password:.*|heat_stack_domain_admin_password: `pwgen 32 1`|g" $USERVARFILE
+sed -i "s|heat_container_mysql_password:.*|heat_container_mysql_password: `pwgen 32 1`|g" $USERVARFILE
+sed -i "s|heat_service_password:.*|heat_service_password: `pwgen 32 1`|g" $USERVARFILE
+sed -i "s|heat_cfn_service_password:.*|heat_cfn_service_password: `pwgen 32 1`|g" $USERVARFILE
 
-grep -q -E "^lb_vip_address" $USERVARFILE || echo -e "\n\nlb_vip_address: 10.51.50.1" >> $USERVARFILE
+sed -i "s|maas_notification_plan:.*|maas_notification_plan: $ADMIN_EMAIL|g" $USERVARFILE
+sed -i "s|maas_agent_token:.*|maas_agent_token: `pwgen 32 1`|g" $USERVARFILE
+sed -i "s|maas_keystone_password:.*|maas_keystone_password: `pwgen 32 1`|g" $USERVARFILE
+
+sed -i "s|neutron_container_mysql_password:.*|neutron_container_mysql_password: `pwgen 32 1`|g" $USERVARFILE
+sed -i "s|neutron_service_password:.*|neutron_service_password: `pwgen 32 1`|g" $USERVARFILE
+
+sed -i "s|nova_container_mysql_password:.*|nova_container_mysql_password: `pwgen 32 1`|g" $USERVARFILE
+sed -i "s|nova_metadata_proxy_secret:.*|nova_metadata_proxy_secret: `pwgen 32 1`|g" $USERVARFILE
+sed -i "s|nova_ec2_service_password:.*|nova_ec2_service_password: `pwgen 32 1`|g" $USERVARFILE
+sed -i "s|nova_service_password:.*|nova_service_password: `pwgen 32 1`|g" $USERVARFILE
+sed -i "s|nova_v3_service_password:.*|nova_v3_service_password: `pwgen 32 1`|g" $USERVARFILE
+sed -i "s|nova_s3_service_password:.*|nova_s3_service_password: `pwgen 32 1`|g" $USERVARFILE
+
+sed -i "s|rpc_support_holland_password:.*|rpc_support_holland_password: `pwgen 32 1`|g" $USERVARFILE
+
+# Think this isn't needed.
+#grep -q -E "^lb_vip_address" $USERVARFILE || echo -e "\n\nlb_vip_address: 10.51.50.1" >> $USERVARFILE
 
 
 
