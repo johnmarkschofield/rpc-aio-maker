96f40e40b3e9b2fb038688fc9cb4417d062c5a99
John Mark Schofield <jms@schof.org>
2014-08-26 10:42:34 -0700

    Per @Aedan in #426 this is fixed. Restarting build to test.

--- config_host_packages.bash
+++ config_host_packages.bash
@@ -9,10 +9,9 @@ set -x
 ssh -o StrictHostKeyChecking=no root@$PUBLIC_IP "true"
 
 
-# Commenting these out due to https://github.com/rcbops/ansible-lxc-rpc/issues/426
-#ssh root@$PUBLIC_IP "cp /etc/apt/sources.list /etc/apt/sources.list.original"
-#ssh root@$PUBLIC_IP "echo deb [arch=amd64] http://dc0e2a2ef0676c3453b1-31bb9324d3aeab0d08fa434012c1e64d.r5.cf1.rackcdn.com LA main > /etc/apt/sources.list"
-#ssh root@$PUBLIC_IP "curl http://dc0e2a2ef0676c3453b1-31bb9324d3aeab0d08fa434012c1e64d.r5.cf1.rackcdn.com/repo.gpg | apt-key add -"
+ssh root@$PUBLIC_IP "cp /etc/apt/sources.list /etc/apt/sources.list.original"
+ssh root@$PUBLIC_IP "echo deb [arch=amd64] http://dc0e2a2ef0676c3453b1-31bb9324d3aeab0d08fa434012c1e64d.r5.cf1.rackcdn.com LA main > /etc/apt/sources.list"
+ssh root@$PUBLIC_IP "curl http://dc0e2a2ef0676c3453b1-31bb9324d3aeab0d08fa434012c1e64d.r5.cf1.rackcdn.com/repo.gpg | apt-key add -"
 
 ssh root@$PUBLIC_IP "apt-get update"
 ssh root@$PUBLIC_IP "DEBIAN_FRONTEND=noninteractive apt-get install -q -y update-notifier-common"
